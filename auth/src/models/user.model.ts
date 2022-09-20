d646c898df88ea7cd1db08e3d15d64b1714f3825
nickLee <ilkinm2003@gmail.com>
2022-09-20 15:54:48 +0400

    Create user model

---
+++ auth/src/models/user.model.ts
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+interface UserAttrs {
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
+
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
+}
+
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      transform(_doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.password;
+      },
+      versionKey: false,
+    },
+  }
+);
+
+userSchema.statics.build = (attrs: UserAttrs) => new User(attrs);
+
+const User = mongoose.model<UserDoc, UserModel>("User", userSchema);
+
+export { User };
