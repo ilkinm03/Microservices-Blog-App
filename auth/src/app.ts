9bd4e4c2af467fb4800769a524ab50c40f6dc528
nickLee <ilkinm2003@gmail.com>
2022-09-20 15:02:00 +0400

    Add new app configurations

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -1,10 +1,25 @@
 import express, { json } from "express";
+import cookieSession from "cookie-session";
 
 import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 
+app.set("trust proxy", true);
 app.use(json());
+app.use(
+  cookieSession({
+    httpOnly: true,
+    signed: false,
+    secure: process.env.NODE_ENV !== "test",
+  })
+);
+
+app.all("*", () => {
+  throw new NotFoundError();
+})
+
 app.use(errorHandler);
 
 export { app };
