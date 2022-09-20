71d58f8013dbbb4b3225ba296a7441df53c6bad7
nickLee <ilkinm2003@gmail.com>
2022-09-20 15:33:20 +0400

    signup test environment initial setup

---
+++ auth/src/routes/__test__/signup.test.ts
@@ -0,0 +1,12 @@
+import request from "supertest";
+import { app } from "../../app";
+
+describe("Signup", () => {
+  it("returns 400 if email is invalid", async () => {});
+
+  it("returns 400 if password is invalid", async () => {});
+
+  it("returns 400 if email is already exists", async () => {});
+
+  it("returns new user on successful signup", async () => {});
+});
---
+++ auth/src/routes/signup.ts
@@ -0,0 +1,9 @@
+import { Router, Request, Response } from "express";
+
+const router = Router();
+
+router.post("/api/auth/signup", (req: Request, res: Response) => {
+  
+});
+
+export { router as signupRouter };
