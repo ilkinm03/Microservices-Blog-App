53fe8bba8171c8a22b843cda1d8a53c0b981f1a5
nickLee <ilkinm2003@gmail.com>
2022-09-20 14:12:04 +0400

    Setup mongodb connection

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,7 +1,17 @@
-import express from "express";
+import mongoose from "mongoose";
+import { app } from "./app";
 
-const app = express();
-
-app.listen(3000, () => {
-  console.log("Listening on port 3000...")
-})
+const start = (async () => {
+  if (!process.env.MONGO_URI) {
+    throw Error("MONGO_URI must be provided!");
+  }
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to DB!");
+  } catch (error) {
+    console.error(error);
+  }
+  app.listen(3000, () => {
+    console.log("Listening on port 3000...");
+  });
+})();
