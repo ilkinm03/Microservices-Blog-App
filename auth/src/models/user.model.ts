61b46adfa4a4b353e3e185572077f826ec326611
nickLee <ilkinm2003@gmail.com>
2022-09-20 16:16:20 +0400

    Add presave hash to user model

--- auth/src/models/user.model.ts
+++ auth/src/models/user.model.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { Password } from "../services/password";
 
 interface UserAttrs {
   email: string;
@@ -38,6 +39,14 @@ const userSchema = new mongoose.Schema(
     },
   }
 );
+
+userSchema.pre("save", async function (done) {
+  if (this.isModified("password")) {
+    const hash = await Password.toHash(this.get("password"));
+    this.set("password", hash);
+  }
+  done();
+});
 
 userSchema.statics.build = (attrs: UserAttrs) => new User(attrs);
 
